5f8cd153ea8f9d5d7dc03d0f20a83b4f5dadc41d
Juan Huina <jhuina@gmail.com>
2025-01-29 23:16:27 -0300

    Se avanza en el desafio amigo secreto, se guarda el dato del input rn una lista y se muestra creando un li

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let amigoAleatorio = Math.floor(Math.random()*amigos);
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+function agregarAmigo() {
+    // se crea una variable para el texto ingresado por el input 
+    let textoIngresado = document.getElementById('amigo').value;   
+    if(textoIngresado === ''){
+        //el usuario dejo vacio el input
+        alert('Por favor, inserte un nombre.');
+        return;
+    }else{
+        //si el nombre ingresado existe debemos indicar ingrese un nombre nuevo de lo contrario cree el li y guarde el dato en esa etiqueta
+        if (amigos.includes(textoIngresado)){
+            alert('el nombre ingresado ya existe, por favor ingrese un nuevo Nombre');
+        }else{         
+            amigos.push(textoIngresado);
+            CrearLi(textoIngresado);
+            //console.log(amigos);
+        }
+    }
+    LimpiarNombre();
+    return;
+}
+function LimpiarNombre(){
+    let valorInput = nombreEscrito = document.querySelector('#amigo');
+    valorInput.value = '';
+}
+// funcion para crear un li en el ul
+function CrearLi(nombre){
+    let nuevoLi = document.createElement('li');
+    // asigno el nombre al li
+    for(i = 0; i < amigos.length; i++){
+        nuevoLi.textContent = nombre;
+        document.querySelector('#listaAmigos').appendChild(nuevoLi);
+    }
+}
+
+function sortearAmigo(){
+    console.log(amigoAleatorio);
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,17 @@
 <h1> Challenge de Principiante en Programación 'Desafio amigo secreto'</h1>
 
 `Inicio del proyecto`
+
+`Inicio del proyecto`
+
+`Se llama los datos ingresados en el input atravez de un identificador id `
+
+`Se deja registrados los datos en un array`
+
+`atravez de un for se crea un li cada  vez que se ingresa un nombre nuevo`
+
+`se valida con no se ingresa datos vacios `
+
+`Se valida que no se reita el nombre`
+
+`Se muestra el nombre en la etiqueta li`
